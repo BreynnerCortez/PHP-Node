ccad5fb9c5f690c71680ff14e0385b9009fcf1ae
Breynner Cortez <breynner0195@gmail.com>
2021-08-30 18:41:22 -0600

    tribal test

--- Backend/Routes/Routes.js
+++ Backend/Routes/Routes.js
@@ -2,10 +2,13 @@ const express = require('express')
 const router = express.Router();
 const controller = require('../Controllers/Controllers');
 
+//GET Requests
 router.route('/get/shows').get(controller.getShows);
 router.route('/get/shows/:id').get(controller.getShow);
+router.route('/get/comments/:id').get(controller.getComments);
+
+//POST Requests
 router.route('/register/user').post(controller.registerUser);
 router.route('/register/comment').post(controller.registerComment)
-router.route('/get/comments/:id').get(controller.getComments);
 
 module.exports = router;
